a87a7a5f4fc2965960999ca11d65c200764e6789
Achintya Tiwary <121821415+achintw@users.noreply.github.com>
2023-08-16 13:59:16 +0530

    Update 1463A_dungeon.cpp

--- 1100/1463A_dungeon.cpp
+++ 1100/1463A_dungeon.cpp
@@ -5,9 +5,18 @@ using namespace std;
 void solve(){
     
     int a,b,c;
-    cin>>a>>b>>c;
-    int s=a+b+c;
-    int EnShots=(a+b+c)/9;
+    cin >> a >> b >> c;
+    int s = a+b+c;
+    int EnShots = (a+b+c)/9;
+
+    /*
+        min death shots = 7 (1 Enhanced shot) => damage dealth = 9
+        so for every EnShot, damage dealth is 9 => #EnShots = (a+b+c)/9
+        (s%9 == 0) means the monsters can be beautifully killed, but now
+        check if all live to die beautifully or get killed before the EnShot
+        (a >= #EnShot && b >= #EnShot && c >= #EnShot)
+        
+    */
     
     if(s%9==0 && a>=EnShots && b>=EnShots && c>=EnShots){
         cout << "YES" << " ";
