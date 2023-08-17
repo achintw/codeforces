956866ed2147e81612ec669783ed43c1fc396b6a
Achintya Tiwary <121821415+achintw@users.noreply.github.com>
2023-08-17 09:59:14 +0530

    Update 1618C_paintTheArray.cpp

--- 1100/1618C_paintTheArray.cpp
+++ 1100/1618C_paintTheArray.cpp
@@ -7,6 +7,13 @@ void solve(){
     cin>>n;
     vector<ll>a(n);
     for(int i=0;i<n;i++) cin>>a[i];
+
+    /*
+        to paint the array alternatively we need
+        to find a number 'd' which divides all of (a1,a3,a5,...)
+        but not (a2,a4,a6,...) or vice-versa, we find a 'd' which 
+        divides the gcd(a1,a3,a5,...) and check if it divides (a2,a4,a6...)
+    */
     
     ll d1=0,d2=0;
     for(int i=0; i<n; i++){
